7c87313fbd61471b45e64295dd6c837c8b35fd68
jjdev4001 <jjdev4001@outlook.com>
2021-01-28 04:03:15 -0500

    Here's my change with directions on how to do it.

--- TestGithubConnection/TestGithubConnection/Program.cs
+++ TestGithubConnection/TestGithubConnection/Program.cs
@@ -12,10 +12,12 @@ namespace TestGithubConnection
         {
             // The code provided will print ‘Hello World’ to the console.
             // Press Ctrl+F5 (or go to Debug > Start Without Debugging) to run your app.
-            Console.WriteLine("Hello World! This REALLY sucks! But it worked twice.");
+            Console.WriteLine("Hello World! This REALLY sucks! But it worked three times.");
             Console.WriteLine("TeamExploerer->Home->Changes->Commit Message.");
-            Console.WriteLine("Push? Something?");
+            Console.WriteLine("Outgoing commits... COMMIT ALL.");
+            Console.WriteLine("Commit made on local repo... SYNC to pushy to the server.");
             Console.WriteLine("SYNC");
+            Console.WriteLine("Then outgoing commits, and PULL.");
             Console.ReadKey();
 
             // Go to http://aka.ms/dotnet-get-started-console to continue learning how to build a console app! 
