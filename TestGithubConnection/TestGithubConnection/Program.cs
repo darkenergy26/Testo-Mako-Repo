3ace133d7760f638a81bbd30efd16a8dde541e52
jjdev4001 <jjdev4001@outlook.com>
2021-01-28 03:55:44 -0500

    Here's the commit message.

--- TestGithubConnection/TestGithubConnection/Program.cs
+++ TestGithubConnection/TestGithubConnection/Program.cs
@@ -12,7 +12,10 @@ namespace TestGithubConnection
         {
             // The code provided will print ‘Hello World’ to the console.
             // Press Ctrl+F5 (or go to Debug > Start Without Debugging) to run your app.
-            Console.WriteLine("Hello World! This REALLY sucks! But it worked once");
+            Console.WriteLine("Hello World! This REALLY sucks! But it worked twice.");
+            Console.WriteLine("TeamExploerer->Home->Changes->Commit Message.");
+            Console.WriteLine("Push? Something?");
+            Console.WriteLine("SYNC");
             Console.ReadKey();
 
             // Go to http://aka.ms/dotnet-get-started-console to continue learning how to build a console app! 
