5dadc1d3b777948ff8e7d5ded4b0dab2b1bd56ed
jjdev4001 <jjdev4001@outlook.com>
2021-01-28 04:15:05 -0500

    The rain in Spain stays mainly in the plain.

--- TestGithubConnection/TestGithubConnection/Program.cs
+++ TestGithubConnection/TestGithubConnection/Program.cs
@@ -17,7 +17,17 @@ namespace TestGithubConnection
             Console.WriteLine("Outgoing commits... COMMIT ALL.");
             Console.WriteLine("Commit made on local repo... SYNC to pushy to the server.");
             Console.WriteLine("SYNC");
-            Console.WriteLine("Then outgoing commits, and PULL.");
+            Console.WriteLine("Then outgoing commits, and PUSH.");
+            Console.WriteLine(" ");
+            Console.WriteLine("(Note that REBASE seemed to solve the 'first push' problem.)");
+            Console.WriteLine(" ");
+            Console.WriteLine("TeamExploerer -> Home-> Changes: Write a commit message.");
+            Console.WriteLine("Light up Commit All.");
+            Console.WriteLine("Output panel feedback 'Commit created locally in repo'.");
+            Console.WriteLine("Team Explorer panel says same, and says SYNC to send to server.");
+            Console.WriteLine("Hit SYNC.");
+            Console.WriteLine("Come to new window (Synchronization?) with Outgoing Commits and 'Push'.");
+            Console.WriteLine("Hit Push and it goes up to Github.");
             Console.ReadKey();
 
             // Go to http://aka.ms/dotnet-get-started-console to continue learning how to build a console app! 
